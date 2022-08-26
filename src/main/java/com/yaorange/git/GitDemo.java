940295d41376c5766349efa53021d087fbaa920d
kevin <kevin@yaorange.com>
2022-08-26 09:13:26 +0800

    push commit1

--- src/main/java/com/yaorange/git/GitDemo.java
+++ src/main/java/com/yaorange/git/GitDemo.java
@@ -8,5 +8,6 @@ public class GitDemo {
         System.out.println("hello hot-fix!");
         System.out.println("master!");
         System.out.println("hot-fix!");
+        System.out.println("push test");
     }
 }
